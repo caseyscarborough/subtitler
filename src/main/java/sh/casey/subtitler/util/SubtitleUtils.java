4f8fe863b3ee4006a9e880773c17f9d548b8019a
Casey Scarborough <caseyscarborough@gmail.com>
2023-07-10 17:20:12 -0400

    Add utility function for checking overlapping subtitles

--- src/main/java/sh/casey/subtitler/util/SubtitleUtils.java
+++ src/main/java/sh/casey/subtitler/util/SubtitleUtils.java
@@ -1,5 +1,7 @@
 package sh.casey.subtitler.util;
 
+import sh.casey.subtitler.model.Subtitle;
+
 public class SubtitleUtils {
 
     public static String convertSrtTimeToAssTime(final String time) {
@@ -8,5 +10,25 @@ public class SubtitleUtils {
 
     public static String convertAssTimeToSrtTime(final String time) {
         return "0" + time.replace(".", ",") + "0";
+    }
+
+    public static boolean overlaps(Subtitle a, Subtitle b) {
+        // Duration of this subtitle overlaps the start time of the other subtitle
+        if (a.getStartMilliseconds() <= b.getStartMilliseconds() && a.getEndMilliseconds() >= b.getStartMilliseconds()) {
+            return true;
+        }
+
+        // Duration of this subtitle overlaps the end time of the other subtitle
+        if (a.getEndMilliseconds() >= b.getEndMilliseconds() && a.getStartMilliseconds() <= b.getEndMilliseconds()) {
+            return true;
+        }
+
+        // Duration of this subtitle is within the other subtitle
+        if (a.getStartMilliseconds() >= b.getStartMilliseconds() && a.getEndMilliseconds() <= b.getEndMilliseconds()) {
+            return true;
+        }
+
+        // Duration of this subtitle completely overlaps the other subtitle on both sides
+        return a.getStartMilliseconds() < b.getStartMilliseconds() && a.getEndMilliseconds() > b.getEndMilliseconds();
     }
 }
