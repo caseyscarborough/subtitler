eb594ef048160d77d09b6bf24512cb508bfb4f0e
Casey Scarborough <caseyscarborough@gmail.com>
2023-08-10 20:30:10 -0400

    Add a filter method without the threshold parameter

--- subtitler/src/main/java/sh/casey/subtitler/filter/SubtitleFilterer.java
+++ subtitler/src/main/java/sh/casey/subtitler/filter/SubtitleFilterer.java
@@ -7,5 +7,9 @@ import java.util.Map;
 
 public interface SubtitleFilterer<T extends SubtitleFile> {
 
+    default void filter(T file, Map<FilterType, List<String>> filters, FilterMode mode) {
+        filter(file, filters, mode, Integer.MAX_VALUE);
+    }
+
     void filter(T file, Map<FilterType, List<String>> filters, FilterMode mode, int threshold);
 }
